88d12afbff9b2384315fc6a031ce76bc71ca6d85
pierre-louis.talbot <pierrelouistalbot@gmail.com>
2021-03-25 16:41:44 +0100

    Addded get last beer and refactored code

--- public/js/script.js
+++ public/js/script.js
@@ -17,28 +17,64 @@ function onSearchBeerSubmit() {
 				steps: ['Sahara', 'grande bretagne', 'Amérique du Nord', 'France'],
 			}
 			console.log(json);
-			document.getElementById('productionYear').innerText = json.productionyear;
-			document.getElementById('origin').innerText = json.origin;
-			document.getElementById('name').innerText = json.name;
-
-			for (let i = 0; i < json.steps.length; i++) {
-				const step = json.steps[i];
-
-				const htmlStep = document.createElement('li');
-				htmlStep.classList.add('row');
-				htmlStep.classList.add('mt-3');
-				htmlStep.classList.add('data-display-row');
-
-				const strong = document.createElement('strong');
-				strong.innerText = `Étape ${i} : `;
-
-				const text = document.createElement('p');
-				text.innerText = step;
-
-				htmlStep.appendChild(strong);
-				htmlStep.appendChild(text);
-				document.getElementById('infosList').appendChild(htmlStep);
-			}
+			fillbeerInfo(json);
 		})
 		.catch((err) => console.error(err));
+}
+
+function getlastBeer() {
+	fetch('/getBeers', {
+		method: 'POST',
+		body: {
+			reference: document.getElementById('beerReference'),
+		},
+	})
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (json) {
+			json = [{
+				productionyear: '2002',
+				origin: 'France',
+				name: 'Triple pression',
+				brand: 'Kro',
+				steps: ['Sahara', 'grande bretagne', 'Amérique du Nord', 'Belgique'],
+			}]
+			console.log(json);
+
+			fillbeerInfo(json[json.length - 1]);
+		})
+		.catch((err) => console.error(err));
+}
+
+function fillbeerInfo(beer) {
+	document.getElementById('infosList').innerHTML = '';
+
+	document.getElementById('infosList').appendChild(createListElement('Année de production :&nbsp;', beer.productionyear));
+	document.getElementById('infosList').appendChild(createListElement('Nom :&nbsp;', beer.name));
+	document.getElementById('infosList').appendChild(createListElement("Pays d'origine :&nbsp;", beer.origin));
+
+	for (let i = 0; i < beer.steps.length; i++) {
+		const step = beer.steps[i];
+
+		document.getElementById('infosList').appendChild(createListElement(`Étape ${i} :&nbsp;`, step));
+	}
+}
+
+function createListElement(title, text) {
+	const htmlElement = document.createElement('li');
+	htmlElement.classList.add('row');
+	htmlElement.classList.add('mt-3');
+	htmlElement.classList.add('data-display-row');
+
+	const strong = document.createElement('strong');
+	strong.innerHTML = title;
+
+	const p = document.createElement('p');
+	p.innerText = text;
+
+	htmlElement.appendChild(strong);
+	htmlElement.appendChild(p);
+
+	return htmlElement;
 }
